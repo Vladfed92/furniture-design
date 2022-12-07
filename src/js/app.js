86b04f88ae9416d701f68df6731d4f0bdcd0a87f
Vladislav Fedorenko <107477959+Vladfed92@users.noreply.github.com>
2022-12-07 12:28:55 +0200

    trying to make splide in different pages

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,46 @@
 import * as flsFunctions from "./modules/functions.js";
 flsFunctions.isWebp();
-
+import Splide from '@splidejs/splide';
 //    BURGER JQERY    //
 
 $(document).ready(function () {
 	$('.burger').click(function (event) {
 		$('.burger,.burger__item,.navigation').toggleClass('active');
 	});
-});
 
-	// SPLIDE SLIDER //
-
-import Splide from '@splidejs/splide';
-
-new Splide('.splide-introduce', '.splide-reviews', {
-	type: 'loop',
-	perPage: 1,
-	pagination: false,
-
+	new Splide('.splide-introduce', {
+		type: 'loop',
+		perPage: 1,
+		pagination: false,
 	}).mount();
 
-new Splide('.splide-reviews', {
-	type: 'loop',
-	perPage: 1,
-	pagination: false,
+	new Splide('.splide-reviews', {
+		type: 'loop',
+		perPage: 1,
+		pagination: false,
+	}).mount();
 
-}).mount();
+
+	new Splide('.splide-goods', {
+		type: 'loop',
+		perPage: 4,
+		pagination: false,
+	}).mount();
+});
+
+// SPLIDE SLIDER //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
