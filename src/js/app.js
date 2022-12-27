22ee288e66bbd5f669db7fc6b059c07b502ecb02
Vladislav Fedorenko <107477959+Vladfed92@users.noreply.github.com>
2022-12-27 11:25:46 +0200

    Add burger on main page with responsive design

--- src/js/app.js
+++ src/js/app.js
@@ -5,28 +5,46 @@ import Splide from '@splidejs/splide';
 
 $(document).ready(function () {
 	$('.burger').click(function (event) {
-		$('.burger,.burger__item,.navigation').toggleClass('active');
+		$('.burger,.burger__item,.navigation,.menu__navigation').toggleClass('active');
 	});
 
-	new Splide('.splide-introduce', {
-		type: 'loop',
-		perPage: 1,
-		pagination: false,
-	}).mount();
+	if ($('.splide-introduce').length > 0) {
+		new Splide('.splide-introduce', {
+			type: 'loop',
+			perPage: 1,
+			pagination: false,
+		}).mount();
+	};
 
-	new Splide('.splide-reviews', {
-		type: 'loop',
-		perPage: 1,
-		pagination: false,
-	}).mount();
+	if ($('.splide-reviews').length > 0) {
+		new Splide('.splide-reviews', {
+			type: 'loop',
+			perPage: 1,
+			pagination: false,
+		}).mount();
+	}
+
+	if ($('.splide-goods').length > 0) {
+		new Splide('.splide-goods', {
+			type: 'loop',
+			perPage: 4,
+			pagination: false,
+			perMove: 1,
+		}).mount();
+	}
 
 
-	new Splide('.splide-goods', {
-		type: 'loop',
-		perPage: 4,
-		pagination: false,
-	}).mount();
+	if ($('.splide-blog-item').length > 0) {
+		new Splide('.splide-blog-item', {
+			type: 'loop',
+			perPage: 2,
+			pagination: false,
+		}).mount();
+	}
 });
+
+
+
 
 // SPLIDE SLIDER //
 
