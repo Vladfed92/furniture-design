26602ce67628ab7407d8c9697b9f7de97ee253c1
Vladislav Fedorenko <107477959+Vladfed92@users.noreply.github.com>
2023-01-26 15:00:39 +0200

    add hover and start making module vindows

--- src/js/app.js
+++ src/js/app.js
@@ -4,13 +4,14 @@ import Splide from '@splidejs/splide';
 //    BURGER JQERY    //
 
 $(document).ready(function () {
-	$('.burger').click(function (event) {
-		$('.burger,.burger__item,.navigation,.menu__navigation').toggleClass('active');
-	});
-	$('.filter-icon').click(function (event) {
-		$('.shop__goods-filter,.filter-icon').toggleClass('active');
-	});
-
+	// реніше робота бургера була забеспечена додаванням класу 'active' за допомогою jqery, наразі сам зробив таку маніпуляцію на чистому JS
+	/*	$('.burger').click(function (event) {
+			$('.burger,.burger__item,.navigation,.menu__navigation').toggleClass('active');
+		});
+		$('.filter-icon').click(function (event) {
+			$('.shop__goods-filter,.filter-icon').toggleClass('active');
+		});
+	*/
 
 	if ($('.splide-introduce').length > 0) {
 		new Splide('.splide-introduce', {
@@ -45,12 +46,65 @@ $(document).ready(function () {
 			pagination: false,
 		}).mount();
 	}
+
 });
 
 
+// Функція додавання класу active по будь якому маркеру (classesForActivation)
+
+const classesForActivation = ['.burger', '.navigation', '.menu__navigation'];
+const domActiveation = [];
+const burger = document.querySelector('.burger');
+
+classesForActivation.forEach(key => {
+	const elementsColection = document.querySelectorAll(key)
+	elementsColection.forEach(element => {
+		domActiveation.push(element)
+	})
+});
+
+const addNewClass = (element) => {
+	element.classList.toggle('active');
+};
+
+burger.addEventListener('click', function () {
+	domActiveation.forEach(addNewClass)
+})
+
+/*
+const funcActivation = (element) => {
+	element.onclick = () => {
+		addNewClass(element);
+	};
+}
+*/
+
+/*
+addNewClass(this) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// Функція додавання класу по маркеру ".js-toggle-active"
+/*
+const addNewClass = (element) => {
+	element.parentElement.classList.toggle('active');
+};
+
+const funcActivation = (element) => {
+	element.onclick = () => {
+		addNewClass(element)
+	}
+}
+
+document.querySelectorAll('.js-toggle-active').forEach(funcActivation)
+*/
 
 
-// SPLIDE SLIDER //
+
+
+
+
+
+
+
 
 
 
